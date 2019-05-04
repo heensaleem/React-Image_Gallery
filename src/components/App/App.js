0330f5d630e2090c287ac474b7787a89d2372b6f
heensaleem <heenakouser440@gmail.com>
2019-05-03 21:59:22 -0500

    get request working displaying object on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+     images : []
+  }
+   // Wait for the first render
+  // Then do things, like call a server
+   componentDidMount() {
+     this.getImages();
+   }
+
+  getImages = () => {
+    axios.get('/gallery')
+    .then(response => {
+      console.log(response.data)
+       // Save this in state
+      // Not spreading, because we want to replace all of tvShowList
+      this.setState({
+        images: response.data
+      })
+    }).catch(err => {
+      console.log(err);
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -9,6 +32,7 @@ class App extends Component {
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br/>
+        <pre>{JSON.stringify(this.state.images)}</pre>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
